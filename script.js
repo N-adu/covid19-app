d280765a064cdd559970b24f4cfda464b5602e28
N Adu <64503636+N-adu@users.noreply.github.com>
2021-07-13 20:01:42 +0100

    Update script.js
    Figured out JavaScript Fetch API for my live stats table. Added it to the project

--- script.js
+++ script.js
@@ -4,25 +4,34 @@ let dataResult = document.getElementById('dataResult');
 async function currentInfo(){
   let url = fetch('https://api.covid19api.com/summary')
   .then(response => response.json())
-  .then(data => console.log(data.Countries[182])) //accessed countries within the object. UK is no.182
-  console.log(url) //accesses covid api object
+  .then(data => data.Countries[182])
+  .then(uk => {
+    console.log(url)
+    let totalCases = uk.TotalConfirmed;
+    let totalRecovery = uk.TotalRecovered;
+    let totalDeaths = uk.TotalDeaths;
+    console.log(totalDeaths)
 
-  // const totalCases = url.TotalConfirmed;
-  // const totalRecovery = url.TotalRecovered;
-  // const totalDeaths = url.TotalDeaths
-  // console.log(url)
-
-  // let tableData1 = document.createElement = ('td');
-  // let tableData2 = document.createElement = ('td');
-  // let tableData3 = document.createElement = ('td');
-
-  // dataResult.appendChild(tableData1);
-  // dataResult.appendChild(tableData2);
-  // dataResult.appendChild(tableData3);
+   let td = document.createElement('td');
+   let td1 = document.createElement('td');
+   let td2 = document.createElement('td');
   
-  };
-  currentInfo();
- 
+   console.log(td)
+  
+   td.innerText = totalCases;
+   dataResult.appendChild(td);
+
+   td1.innerText = totalRecovery;
+   dataResult.appendChild(td1);
+
+   td2.innerText = totalDeaths;
+   dataResult.appendChild(td2);
+    
+  
+  }) 
+};
+
+currentInfo();
   
 // time
 const hours = document.getElementById('hours');
